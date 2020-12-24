6886298cc9127d59123276e2c2c453c9b94544ff
kgulyaev <71878401+kgulyaev@users.noreply.github.com>
2020-12-24 19:44:08 +0300

    Update __init__.py
    fix import

--- shifted_beta_geometric/__init__.py
+++ shifted_beta_geometric/__init__.py
@@ -1,5 +1,6 @@
 from .sbg import derl
 from .sbg import fit
+from .sbg import fit_predict
 from .sbg import fit_multi_cohort
 from .sbg import predicted_retention
 from .sbg import predicted_survival
